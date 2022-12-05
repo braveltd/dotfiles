c18b4a56262087a8d3b3f83827e0ef15d6075bb4
bravelzt <belicoff0505@gmail.com>
2022-12-05 07:18:44 +0300

    somethink bruhhh

---
+++ .config/awesome/awesome/widgets/wibar/network.lua
@@ -0,0 +1,28 @@
+-- not done yet
+local wibox = require("wibox")
+
+local networkWidget = wibox.widget({
+	{
+		image = "/home/brave/.config/awesome/images/wibar/network/signal-cellular-2.svg",
+		resize = true,
+		widget = wibox.widget.imagebox,
+	},
+	{
+		text = " ",
+		widget = wibox.widget.textbox,
+	},
+	layout = wibox.layout.fixed.horizontal
+})
+
+--[[
+local networkWidget = wibox.widget{
+	image = "/home/brave/.config/awesome/images/wibar/network/signal-cellular-2.svg",
+	widget = wibox.widget.imagebox,
+}
+--]]
+
+_G.networkWidgetContainer = wibox.container.background(networkWidget)
+_G.networkWidgetContainer.bg = "#a6e3a1"
+_G.networkWidgetContainer.fg = "#1e1e2e"
+
+return _G.networkWidgetContainer
