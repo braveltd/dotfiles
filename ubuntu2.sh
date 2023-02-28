e75f7e494b34ce2bff1a8a2bdabd4893a1c725b2
Brave <github@bravestd.com>
2023-02-28 08:57:55 +0300

    ubuuntuu sahdashdhas

--- ubuntu2.sh
+++ ubuntu2.sh
@@ -7,15 +7,23 @@ touch ~/.zshenv
 
 cp .oh-my-zsh/themes/brave.zsh-theme ~/.oh-my-zsh/themes/
 
-# download rbenv from github, activation already in .zshrc
+# download rbenv from github,
+# activation already in .zshrc
+# download ruby-build plugin
 git clone https://github.com/rbenv/rbenv.git ~/.rbenv
+git clone git@github.com:rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
 
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/
+rbenv install 3.2.1
+rbenv global 3.2.1
+
+gem install colorls
+
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
 
 git clone https://github.com/NvChad/NvChad ~/.config/nvim --depth 1
 cp -r ./.config/nvim/lua/custom/ ~/.config/nvim/lua/
 
-git clone https://github.com/gpakosz/.tmux.git ~/
+git clone https://github.com/gpakosz/.tmux.git ~/.tmux
 ln -s -f ~/.tmux/.tmux.conf
 
