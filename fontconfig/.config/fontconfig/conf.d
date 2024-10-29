145d66886a9c097fb3fbcd771f9f67eb419e77ee
braveltd <belicoff0505@gmail.com>
2024-10-29 09:53:55 +0300

    Added fontconfig

---
+++ fontconfig/.config/fontconfig/conf.d
@@ -0,0 +1,19 @@
+<?xml version='1.0'?>
+<!DOCTYPE fontconfig SYSTEM 'urn:fontconfig:fonts.dtd'>
+<fontconfig>
+  <match target="font">
+    <edit mode="assign" name="rgba">
+      <const>none</const>
+    </edit>
+  </match>
+  <alias>
+    <family>JetBrainsMono Nerd Font Emoji Uni</family>
+    <prefer>
+      <family>JetBrainsMono Nerd Font</family>
+      <family>Apple Color Emoji</family>
+      <family>FreeSerif</family>
+      <family>Unifont</family>
+    </prefer>
+  </alias>
+ <dir>~/.local/share/fonts</dir>
+</fontconfig>
